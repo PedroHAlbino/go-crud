3db86c0ae5ad879069f3c1a1c79b640b7a159367
PedroAlbino <pedro.albino@consulticom.com.br>
2022-10-21 15:38:34 -0300

    CRUD FINALIZADO

--- models/Produtos.go
+++ models/Produtos.go
@@ -2,11 +2,10 @@ package models
 
 import (
 	"banco/db"
-	"log"
 )
 
 type Produto struct {
-	id         int
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -36,6 +35,7 @@ func BuscaTodosOsProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = float64(preco)
@@ -52,14 +52,71 @@ func BuscaTodosOsProdutos() []Produto {
 func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	db := db.Conecta()
 
-	insereDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao,preco, quantidade) values($1,$2,$3,$4")
+	insereDadosNoBanco, err := db.Prepare("insert into produtos(nome, descricao,preco, quantidade) values (?,?,?,?)")
 	if err != nil {
 		panic(err.Error())
 	}
 
 	insereDadosNoBanco.Exec(nome, descricao, preco, quantidade)
 
-	log.Println(insereDadosNoBanco)
+	defer db.Close()
+}
 
+func DeletaProduto(id string) {
+	db := db.Conecta()
+
+	deletarProduto, err := db.Prepare("delete from produtos where id = ?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deletarProduto.Exec(id)
+
+	defer db.Close()
+
+}
+
+func EditaProduto(id string) Produto {
+	db := db.Conecta()
+
+	produtoDoBanco, err := db.Query("select * from produtos where id = ?", id)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produtoParaAtualizar := Produto{}
+
+	for produtoDoBanco.Next() {
+		var id int
+		var nome, descricao string
+		var preco float64
+		var quantidade int64
+
+		err = produtoDoBanco.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produtoParaAtualizar.Id = id
+		produtoParaAtualizar.Nome = nome
+		produtoParaAtualizar.Descricao = descricao
+		produtoParaAtualizar.Preco = preco
+		produtoParaAtualizar.Quantidade = quantidade
+
+	}
+	defer db.Close()
+	return produtoParaAtualizar
+
+}
+
+func AtualizaProduto(id int, nome, descricao string, preco float64, quantidade int) {
+	db := db.Conecta()
+
+	AtualizaProduto, err := db.Prepare("update produtos set nome=?, descricao=?, preco=?, quantidade=? where id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	AtualizaProduto.Exec(nome, descricao, preco, quantidade, id)
 	defer db.Close()
 }
